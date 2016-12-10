323e3797803e188183fc52a6518119f2432d0af5
Paramananda Ponnaiyan <Paramananda>
2016-12-10 13:37:35 +0530

    need to change directory first.

--- wsgi/myproject/utils/updatesecrets.sh
+++ wsgi/myproject/utils/updatesecrets.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 CLIENT_ID=$1
 CLIENT_SECRET=$2
-
+cd app-root/repo/wsgi/myproject
 echo "CLIENT_ID='${CLIENT_ID}'" >> myproject/settings.py
 echo "CLIENT_SECRET='${CLIENT_SECRET}'" >> myproject/settings.py
