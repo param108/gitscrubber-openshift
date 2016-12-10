0d2012e442413a8f472275b094480e4301d8c93b
Paramananda Ponnaiyan <Paramananda>
2016-12-10 13:07:53 +0530

    changing the @ to ~ in the GET request.

--- wsgi/myproject/issueview/views.py
+++ wsgi/myproject/issueview/views.py
@@ -202,7 +202,7 @@ def issues_show(request, owner, board):
     if filt != "":
       issue_list = apply_filter(issue_list, str(filt))
     randomstr = ''.join(random.choice(string.letters) for i in xrange(10))
-    board_state_secret=str(boards[0].id)+"@"+str(filt)+"@"+randomstr
+    board_state_secret=str(boards[0].id)+"~"+str(filt)+"~"+randomstr
     oauth_details = OauthCheck()
     oauth_details.user = request.user
     oauth_details.state = board_state_secret
@@ -279,8 +279,8 @@ def issues_authorize(request):
     #fully authorized
     access_token = response["access_token"]
     token_type = response["token_type"]
-    boardid = state.split("@")[0]
-    filt = state.split("@")[1]
+    boardid = state.split("~")[0]
+    filt = state.split("~")[1]
 
     board = Board.objects.get(pk=boardid)
     REPOS=[x.repository for x in Repository.objects.filter(board=boards[0])]
