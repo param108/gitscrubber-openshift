94da277611b877f4804d982b5840dd7182b3b176
Paramananda Ponnaiyan <Paramananda>
2016-12-11 06:40:29 +0530

    Need to handle empty inputs for the filter

--- wsgi/myproject/issueview/forms.py
+++ wsgi/myproject/issueview/forms.py
@@ -10,9 +10,9 @@ class Userform(forms.Form):
   username=forms.CharField(max_length=100)
 
 class Filterform(forms.Form):
-  release=forms.CharField(max_length=50)
-  assigned=forms.CharField(max_length=50)
-  repository=forms.CharField(max_length=50)
-  status=forms.CharField(max_length=50)
+  release=forms.CharField(max_length=50,required=False)
+  assigned=forms.CharField(max_length=50,required=False)
+  repository=forms.CharField(max_length=50,required=False)
+  status=forms.CharField(max_length=50,required=False)
 
 FILTER_OPTIONS = ["release","assigned","repository","status"]
